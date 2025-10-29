70a0694863db2c288c54f49dfa7900f130d384ef
Brian Chew <brian@bchewy.com>
2025-10-29 13:54:26 +0800

    Feat: add revalidation for Home component to improve data freshness

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { getLeaderboard, getOneWordLeaderboard, getTypeRacerLeaderboard, getHistory } from "@/lib/leaderboard";
 import HomeContent from "@/components/HomeContent";
 
+export const revalidate = 60;
+
 export default async function Home() {
   const speedEntries = await getLeaderboard(10);
   const oneWordEntries = await getOneWordLeaderboard(10);
